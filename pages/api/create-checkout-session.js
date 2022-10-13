8c413a1df6eb0e0a9603d48fab28ff24d3ee6510
Srijan Dubey <87586713+Srijan-D@users.noreply.github.com>
2022-10-13 23:22:59 +0530

    Update create-checkout-session.js
    updated the file with some cahnges

--- pages/api/create-checkout-session.js
+++ pages/api/create-checkout-session.js
@@ -1,12 +1,9 @@
 
 
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-// eslint-disable-next-line import/no-anonymous-default-export
+
 export default async (req, res) => {
     const { items, email } = req.body;
-    console.log(items);
-    console.log(email);
-    //stripe accepts data items in this format that is why we have to transform it
     const transformedItems = items.map((item) => ({
         quantity: 1,
         price_data: {
@@ -19,21 +16,21 @@ export default async (req, res) => {
             }
         }
     }));
+    
     const session = await stripe.checkout.sessions.create({
         payment_method_types: ['card'],
-        // shipping_rates: ['shr_1LrkUVSAsh4SL9T9a8Pn5xoX'],
         shipping_address_collection: {
-            allowed_countries: ['IN', 'US', 'CA'],
+            allowed_countries: ['IN'],
         },
         line_items: transformedItems,
         mode: 'payment',
-
         success_url: `${process.env.HOST}/success`,
         cancel_url: `${process.env.HOST}/checkout`,
         metadata: {
             email,
         }
     })
+    
     res.status(200).json({ id: session.id });
 
 }
